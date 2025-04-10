6ae118b197195d052f712b0540f7eaa12626f5f3
psstoykov <145485240+psstoykov@users.noreply.github.com>
2025-04-10 11:14:11 +0200

    follow completed

--- src/components/follow-page/FollowPage.jsx
+++ src/components/follow-page/FollowPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./FollowPage.css";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -12,12 +12,42 @@ const FollowPage = () => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        getFollowStats(uid, followType).then((doc) => {
-            setUsers(doc);
+        getFollowStats(uid, followType).then((users) => {
+            users.sort((a, b) => a.username.localeCompare(b.username)); //sort alpabetically
+            const filteredArray = users.filter((a) => a.uid !== uid); //filter self account out
+
+            setUsers(filteredArray);
         });
     }, []);
-    console.log(users);
-    return <></>;
+
+    if (users.length == 0) {
+        return (
+            <>
+                <h1 className="user-list-title">List of {followType}</h1>
+                <div className="users-list">
+                    <div className="user-list-field">The list is empty</div>
+                </div>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <h1 className="user-list-title">List of {followType}</h1>
+                <div className="users-list">
+                    {users.map((user) => (
+                        <div className="user-list-field" key={user.createdAt}>
+                            <Link
+                                to={"/message/" + user.uid}
+                                className="msg-btn"
+                            >
+                                {user.username}
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </>
+        );
+    }
 };
 
 export default FollowPage;
